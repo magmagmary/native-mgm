228121372f3abfebf3e1361e4f66e817bd26b0e4
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-02-21 15:08:24 +0100

    chore: page press handler

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Index() {
   return (
     <div className="bg-gray-950 h-svh w-svw text-white flex flex-col gap-6 items-center justify-center text-3xl">
       Page2
-      <Button title='title' />
+      <Button title='title' onPress={() => alert('hi')} />
     </div>
   );
 }
