f3a6bdd78de330c4edb66db7a34cd4ac82db8437
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-02-21 15:19:33 +0100

    make expo app work with tailwind

--- apps/expo-mgm/metro.config.js
+++ apps/expo-mgm/metro.config.js
@@ -2,41 +2,40 @@ const { withNxMetro } = require('@nx/expo');
 const { getDefaultConfig } = require('@expo/metro-config');
 const { mergeConfig } = require('metro-config');
 const { withNativeWind } = require('nativewind/metro');
-const path = require('path');
 
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.watchFolders = [path.resolve(__dirname, '../../')];
-defaultConfig.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, 'node_modules'),
-  path.resolve(__dirname, '../../node_modules'),
-];
-const { assetExts, sourceExts } = defaultConfig.resolver;
+module.exports = Promise.resolve(getDefaultConfig(__dirname))
+  .then((defaultConfig) => {
+    const { assetExts, sourceExts } = defaultConfig.resolver;
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const customConfig = {
-  cacheVersion: 'expo-mgm',
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-  resolver: {
-    assetExts: assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...sourceExts, 'cjs', 'mjs', 'svg', 'css'],
-  },
-};
+    /**
+     * Metro configuration
+     * https://reactnative.dev/docs/metro
+     *
+     * @type {import('metro-config').MetroConfig}
+     */
+    const customConfig = {
+      cacheVersion: 'monorepo-nx',
+      transformer: {
+        babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      },
+      resolver: {
+        assetExts: assetExts.filter((ext) => ext !== 'svg'),
+        sourceExts: [...sourceExts, 'cjs', 'mjs', 'svg'],
+      },
+    };
 
-const mergedConfig = mergeConfig(defaultConfig, customConfig);
+    return withNxMetro(mergeConfig(defaultConfig, customConfig), {
+      // Change this to true to see debugging info.
+      // Useful if you have issues resolving modules
+      debug: false,
+      // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
+      extensions: [],
+      // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
+      watchFolders: [],
+    });
+  })
+  .then((withNx) => {
+    const finalConfig = withNativeWind(withNx, { input: './global.css' });
 
-const nativeWindConfig = withNativeWind(mergedConfig, {
-  input: './global.css',
-});
-
-module.exports = withNxMetro(nativeWindConfig, {
-  debug: false,
-  extensions: [],
-  watchFolders: [],
-});
+    return finalConfig;
+  });
--- apps/expo-mgm/tailwind.config.js
+++ apps/expo-mgm/tailwind.config.js
@@ -1,9 +1,14 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [require('../../tailwind.config'), require('nativewind/preset')],
   content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Expo app files
+    join(__dirname, './src/**/*.{ts,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
   ],
+
   theme: {
     extend: {},
   },
