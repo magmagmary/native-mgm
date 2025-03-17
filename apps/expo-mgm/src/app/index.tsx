767b03633f9e9404a6f8643bd55376b556f10d1c
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-03-17 17:33:58 +0100

    feat: expo image

--- apps/expo-mgm/src/app/index.tsx
+++ apps/expo-mgm/src/app/index.tsx
@@ -1,8 +1,10 @@
 import { Text, View } from "react-native";
+import { Image } from "expo-image";
 
 export default function Page() {
 	return (
 		<View className="bg-white w-full h-full flex items-center justify-center">
+			<Image source={require("../../assets/icon.png")} className="w-10 h-10 " />
 			<Text className="text-primary text-4xl">Hello there</Text>
 		</View>
 	);
