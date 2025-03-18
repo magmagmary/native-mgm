16e54ac090acb134fe0dce7e21bee04d7ad17bf6
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-03-18 14:46:13 +0100

    feat: make search bar filter the results

--- apps/expo-restaurant/src/app/index.tsx
+++ apps/expo-restaurant/src/app/index.tsx
@@ -3,17 +3,25 @@ import { Text, View } from "react-native";
 import SearchBar from "../components/home/SearchBar";
 import { useGetPostsQuery } from "@native-magmag/service";
 import Posts from "../components/home/Posts";
+import { useDebounceValue } from "usehooks-ts";
 
 export const Index = () => {
 	const { data, isLoading } = useGetPostsQuery();
 	const [search, setSearch] = useState("");
+	const [debouncedSearchValue] = useDebounceValue(search, 500);
 
-	console.log(data);
+	const filteredData = data?.filter((post) =>
+		post.title.toLowerCase().includes(debouncedSearchValue.toLowerCase()),
+	);
 
 	return (
 		<View className="w-svw h-svh">
 			<SearchBar search={search} setSearch={setSearch} />
-			{isLoading || !data ? <Text>Loading...</Text> : <Posts posts={data} />}
+			{isLoading || !filteredData ? (
+				<Text>Loading...</Text>
+			) : (
+				<Posts posts={filteredData} />
+			)}
 		</View>
 	);
 };
