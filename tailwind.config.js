9f0b17b64077d48b864e0621c11e1ab6a6b9d035
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-03-20 17:15:42 +0100

    cleanup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './apps/expo-mgm/**/*.{js,jsx,ts,tsx}', // Expo app files
-    './apps/web/**/*.{js,jsx,ts,tsx}', // Next.js app files
-    './apps/native-mgm/**/*.{js,jsx,ts,tsx}', // Next.js app files
-    './libs/ui/**/*.{js,jsx,ts,tsx}', // Shared library files
+    './apps/**/*.{js,jsx,ts,tsx}', // Expo app files
+    './libs/**/*.{js,jsx,ts,tsx}', // Shared library files
   ],
   theme: {
     extend: {
