897b0391d6f76f331f6d7795c781bbe556a1323d
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-03-25 11:50:47 +0100

    Icons

--- apps/expo-shopping-list/src/app/(index)/index.tsx
+++ apps/expo-shopping-list/src/app/(index)/index.tsx
@@ -2,14 +2,42 @@ import React from 'react';
 import Button from '../../components/button';
 import { BodyScrollView } from '../../components/BodyScrollView';
 import { useClerk } from '@clerk/clerk-expo';
+import { Stack } from 'expo-router';
+import { Pressable, View } from 'react-native';
+import { IconSymbol } from '../../components/IconSymbol';
+import { appleBlue } from '../../constants/colors';
 
 const Home = () => {
   const { signOut } = useClerk();
 
+  const rightHeader = () => (
+    <View>
+      <Pressable>
+        <IconSymbol name="plus" color={appleBlue} />
+      </Pressable>
+    </View>
+  );
+  const leftHeader = () => (
+    <View>
+      <Pressable>
+        <IconSymbol name="gear" color={appleBlue} />
+      </Pressable>
+    </View>
+  );
+
   return (
-    <BodyScrollView contentContainerStyle={{ padding: 16 }}>
-      <Button onPress={signOut}>Sign Out</Button>
-    </BodyScrollView>
+    <>
+      <Stack.Screen
+        options={{
+          title: 'Home',
+          headerRight: rightHeader,
+          headerLeft: leftHeader,
+        }}
+      />
+      <BodyScrollView contentContainerStyle={{ padding: 16 }}>
+        <Button onPress={signOut}>Sign Out</Button>
+      </BodyScrollView>
+    </>
   );
 };
 
